86b796570255af4c58b0f31dad9d81ff69626721
KaiStar626 <eknots14@gmail.com>
2022-07-05 20:13:57 -0400

    set up functionality for post/submit button

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,14 +1,36 @@
 import {useForm} from "react-hook-form";
-import React, {Fragment, useState, Component, forwardRef}from "react";
+import React, {Fragment, useState, Component, forwardRef, useEffect}from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const CommentForm = forwardRef( 
 	({ initName, initComment, initTitle, onSubmit}, ref) =>
 	{
-	
-	const [isSending, setIsSending] = useState(false);
+		const[username, setName] = useState(initName)
+		const[comment, setComment] = useState(initComment)
+		const[title, setTitle] = useState(initTitle)
+		const[isValidInput, setValidInput] = useState(initName && initComment && initTitle)
+
+	useEffect(() => {
+		const validInput = username.length && text.length;
+		if (isValidInput!==validInput) setValidInput(isValidInput);
+		}, [username, comment, title]);
+
+		function postSubmit(e) {
+			SubmitEvent.textContent = 'time stamp: ${e.timeStamp}';
+			e.preventDefault();
+		}
+		const post = document.getElementById('submit');
+
+
+		const editName=e=> setName(e.target.value);
+		const editComment=e=> setComment(e.target.value);
+		const editTitle=e=> setTitle(e.target.value);
+
+	{/*const [isSending, setIsSending] = useState(false);*/}
 	const {register, errors, handleSubmit, reset} = useForm();
 
-	const onSubmit = data => {
+	{
+		/*const onSubmit = data => {
 		setIsSending(true);
 		
 		fetch("/api/Examplecomments", {
@@ -20,14 +42,25 @@ const CommentForm = forwardRef(
 				setIsSending(false);
 			} 
 		})
+	     }
+         */
+    }
+
+	const postSubmit = () => {
+		onSubmit(username.replace(/\s/g, " ", "-"), comment, title);
+		setName(" ");
+		setComment(" ");
+		setTitle(" ");
 	}
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
+		<form {/*onSubmit={handleSubmit(onSubmit)}*/}>
 			<input 
+			autoFocus
 			name='name' 
 			type='text' 
 			placeholder='Name' 
 			ref={register({required: true, maxLength: 80})}
+			value={username}
 			/>
 
 			<input 
@@ -35,6 +68,7 @@ const CommentForm = forwardRef(
 			type='text' 
 			placeholder='Title' 
 			ref={register({required: false, maxLength: 80})}
+			value={title}
 			/>
 
 			<textarea 
@@ -43,19 +77,32 @@ const CommentForm = forwardRef(
 			rows='10' 
 			placeholder='Comment'
 			ref={register({required: true, maxLength: 500})}
-			/>
+			></textarea>
 
 			{errors.comment && (<span> You have to write a comment</span>)}
 			
 
-			<input
+			{/*<input
 			type="submit"
 			disabled={isSending}
 			value={isSending ? "Posting Comment " : "Post"}
-			/>
-			{/*<button type='button'>Submit</button>*/}
+            />*/}
+
+			<button 
+				disabled={!isValidInput}  onClick={postSubmit} type='button'>
+				<strong>Submit</strong>
+				<FontAwesomeIcon icon="fa-solid fa-rocket" />
+			</button>
+
 		</form>
-	);
-}
+		);
+	}
+);
  
+CommentForm.defaultProps = {
+	initName= " ",
+	initTitle= " ",
+	initTitle= " "
+}
+
 export default CommentForm;
